71ef1f84a6911df701642e2541d8e715a964dcf2
CutieMath <cutie.math@protonmail.com>
2022-05-04 20:25:57 +1000

    finished styling the feedback items

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
 		{
 			id: 1,
 			rating: 9,
-			content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.'
+			content: 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?'
 		},
 		{
 			id: 2,
@@ -20,7 +20,7 @@
 	]
 </script>
 
-<main>
+<main class="container">
 	<FeedbackList feedbacks={feedbacks} />
 </main>
 
